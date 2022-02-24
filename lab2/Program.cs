87881df74572d927b80e1b8cf9887911db18813a
Илья Харченко <kharchenko_ilia@mail.ru>
2022-02-24 13:00:11 +0700

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Roman;
+
+namespace lab1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RomanNumber rn1 = new RomanNumber(71);
+            RomanNumber rn2 = (RomanNumber)rn1.Clone();
+            //rn2 = RomanNumber.Add(rn1, rn2);
+            Console.WriteLine($"{rn1.ToString()}  {rn2.ToString()} {rn1.CompareTo(rn2)}");
+        }
+    }
+}
---
+++ lab2/RomanNumberException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman
+{
+    public class RomanNumberException:Exception
+    {
+        public RomanNumberException() { }
+        public RomanNumberException(string message):base(message) { }
+
+    }
+}
