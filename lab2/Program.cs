1ae4252ce27eb668bc85c0fff5344b339c92da04
Илья Харченко <kharchenko_ilia@mail.ru>
2022-02-24 19:48:25 +0700

    Displaying actions on the screen

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using Roman;
 
-namespace lab1
+namespace lab2
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            RomanNumber rn1 = new RomanNumber(71);
-            RomanNumber rn2 = (RomanNumber)rn1.Clone();
-            //rn2 = RomanNumber.Add(rn1, rn2);
-            Console.WriteLine($"{rn1.ToString()}  {rn2.ToString()} {rn1.CompareTo(rn2)}");
+            ushort[] number = { 3, 459, 1002, 84, 132 };
+            RomanNumber[] rn = { new RomanNumber(number[0]), new RomanNumber(number[1]),
+                new RomanNumber(number[2]), new RomanNumber(number[3]), new RomanNumber(number[4]) };
+
+            for (int i = 0; i < 5; i++)
+                Console.Write($"{number[i]}\t");
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+                Console.Write($"{rn[i].ToString()}\t");
+
+            Console.WriteLine($"\n\nСумма:\t {number[0]} + {number[1]} = {number[0] + number[1]}");
+            Console.WriteLine($"\t {rn[0].ToString()} + {rn[1].ToString()} = {RomanNumber.Add(rn[0], rn[1]).ToString()}");
+
+            Console.WriteLine($"\n\nРазность:\t {number[1]} - {number[0]} = {number[1] - number[0]}");
+            Console.WriteLine($"\t\t {rn[1].ToString()} - {rn[0].ToString()} = {RomanNumber.Sub(rn[1], rn[0]).ToString()}");
+
+            Console.WriteLine($"\n\nУмножение:\t {number[0]} * {number[1]} = {number[0] * number[1]}");
+            Console.WriteLine($"\t\t {rn[0].ToString()} * {rn[1].ToString()} = {RomanNumber.Mul(rn[1], rn[0]).ToString()}");
+
+            Console.WriteLine($"\n\nДеление:\t {number[1]} / {number[0]} = {number[1] / number[0]}");
+            Console.WriteLine($"\t\t {rn[1].ToString()} / {rn[0].ToString()} = {RomanNumber.Div(rn[1], rn[0]).ToString()}");
+
+            Console.WriteLine("\n\nОтсортированный массив:");
+            Array.Sort(rn);
+            Array.Sort(number);
+            for (int i = 0; i < 5; i++)
+                Console.Write($"{number[i]}\t");
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+                Console.Write($"{rn[i].ToString()}\t");
+            Console.WriteLine();
         }
     }
 }
